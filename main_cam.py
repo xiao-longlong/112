de93f47a3a7da4569a06885580a892e222e841d9
Cao Qing xin <95917985+Sparks3579@users.noreply.github.com>
2024-08-09 19:09:49 +0800

    Update main_cam.py

--- main_cam.py
+++ main_cam.py
@@ -1,27 +1,17 @@
-import os
 import cv2
-import time
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-import numpy as np
-
-# 加载模型组件库
-from components.hand_detect.yolo_v3_hand import yolo_v3_hand_model
-from components.hand_keypoints.handpose_x import handpose_x_model
-# from components.hand_track.hand_track import Tracker
-from components.hand_track.hand_track import pred_gesture
-from lib.hand_lib.utils.utils import parse_data_cfg
-
-from components.hand_gesture.resnet import resnet18
-
 import torch
 import argparse
 
+from components.hand_detect.yolo_v3_hand import yolo_v3_hand_model
+from components.hand_keypoints.handpose_x import handpose_x_model
+from components.hand_track.hand_track import pred_gesture
+from lib.hand_lib.utils.utils import parse_data_cfg
+from components.hand_gesture.resnet import resnet18
 
-def handpose_x_process(config, is_videos, video_path):
+
+def handpose_x_process(config, video_path):
     # 模型初始化
     print("load model component  ...")
-    # yolo v3 手部检测模型初始化
     if config["detect_model_arch"] == "yolo_v3":
         hand_detect_model = yolo_v3_hand_model(
             conf_thres=float(config["detect_conf_thres"]), nms_thres=float(config["detect_nms_thres"]), model_arch=config["detect_model_arch"], model_path=config["detect_model_path"]
@@ -45,7 +35,25 @@ def handpose_x_process(config, is_videos, video_path):
         print("Error: Could not open video.")
         return
 
+    gesture_to_number = {
+        "fist": 0,
+        "one": 1,
+        "yearh": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "heartSingle": 7,
+        "gun": 8,
+        "thunbUp": 9,
+    }
+
+    gesture_history = []
+    output_flag = []
+    four_same_gestures_confirmed = 0  # 用于记录确认的四次相同十帧手势
+
     while True:
+        # if four_same_gestures_confirmed < 4:
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -77,38 +85,51 @@ def handpose_x_process(config, is_videos, video_path):
             # 预测手指关键点
             pts_ = handpose_model.predict(algo_img[y1:y2, x1:x2, :])
             gesture_name = pred_gesture(box, pts_, frame, gesture_model)  # 确保传入frame而不是algo_img
-            print(gesture_name)
 
-            # 在原始帧上绘制矩形和标签
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, "LABEL {}".format(gesture_name), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+            print(gesture_name)  # 这个地方需要修改一下，首先打印的内容需要修改改为某种编码，其次需要将这个编码传递给其他模块
+            gesture_history.append(gesture_name)
 
-        # 显示结果
-        cv2.imshow("image", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):  # 按'q'退出
-            break
+            if len(gesture_history) >= 20 and gesture_history[-20:].count(gesture_history[-1]) == 20:  # 映射手势名称到数字
 
-    # 释放资源
-    cap.release()
-    cv2.destroyAllWindows()
+                gesture_number = gesture_to_number.get(gesture_history[-1], 0)
+                print("连续10帧相同，手势数字:", gesture_number)
+                output_flag.append(gesture_number)
+                # 重置手势历史记录
+                gesture_history = []
+                four_same_gestures_confirmed += 1
+                print("第x次:", four_same_gestures_confirmed)
+
+                # 检查是否达到四次连续三帧相同手势的识别结果
+                if len(output_flag) >= 4:
+                    recent_four = output_flag[:4]
+                    result = "".join(str(num) for num in recent_four)  # 将数字列表转换为字符串
+                    print("四次连续结果:", result)
+                    break
+
+        #     # 在原始帧上绘制矩形和标签
+        #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        #     cv2.putText(frame, "LABEL {}".format(gesture_name), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        #     break
+
+        # # 显示结果
+        # cv2.imshow("image", frame)
+        # if cv2.waitKey(1) & 0xFF == ord("q"):  # 按'q'退出
+        #     break
+
+        if four_same_gestures_confirmed >= 4:
+            print("已确认四次连续十帧相同手势，退出程序。")
+            break  # 退出循环
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=" Project Hand Pose Inference")
     parser.add_argument("--cfg_file", type=str, default="lib/hand_lib/cfg/handpose.cfg", help="model_path")  # 模型路径
-    # parser.add_argument(
-    #     "--test_path", type=str, default="/mnt/data/RecogniseSIgn/simple-handpose-recognition/test_set", help="test_path"
-    # )  # 测试图片路径 'weights/handpose_x_gesture_v1/handpose_x_gesture_v1/000-one' camera_id
-    parser.add_argument("--is_video", type=bool, default=True, help="if test_path is video")  # 是否视频
     parser.add_argument("--video_path", type=str, default="0", help="0 for cam / path ")  # 是否视频
 
     print("\n/******************* {} ******************/\n".format(parser.description))
     args = parser.parse_args()  # 解析添加参数
-
     config = parse_data_cfg(args.cfg_file)
-    is_videos = args.is_video
-    # test_path = args.test_path
     video_path = args.video_path
 
-    handpose_x_process(config, is_videos, video_path)
+    handpose_x_process(config, video_path)
